19a8cece0fb07c7996f74ec38e4d56634fa0dade
triveja <pstriveja97@gmail.com>
2022-10-31 11:30:43 +0530

    First commit

---
+++ src/main/java/vrmanage/versionmanage2/VersionManage2Application.java
@@ -0,0 +1,13 @@
+package vrmanage.versionmanage2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionManage2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionManage2Application.class, args);
+	}
+
+}
---
+++ src/test/java/vrmanage/versionmanage2/VersionManage2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vrmanage.versionmanage2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionManage2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
